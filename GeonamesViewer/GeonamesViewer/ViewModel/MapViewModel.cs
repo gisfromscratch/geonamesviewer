c299b5b9de0d3308c37210e72dc8ad2a2d30c56e
J.T <gisfromscratch@live.de>
2018-02-25 13:28:44 +0100

    #1: Using a vector tiled basemap and a graphics overlay

---
+++ GeonamesViewer/GeonamesViewer/ViewModel/MapViewModel.cs
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2018 Jan Tschada
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+using Esri.ArcGISRuntime;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.UI;
+
+namespace GeonamesViewer.ViewModel
+{
+    /// <summary>
+    /// Provides map data to the application
+    /// </summary>
+    public class MapViewModel : INotifyPropertyChanged
+    {
+        private readonly Basemap _basemap;
+        private Map _map;
+        private GraphicsOverlayCollection _overlays;
+        private GraphicsOverlay _geonamesOverlay;
+
+        public MapViewModel()
+        {
+            // Initialize a new basemap
+            _basemap = Basemap.CreateTopographicVector();
+            var status = _basemap.LoadStatus;
+            switch (status)
+            {
+                case LoadStatus.Loaded:
+                    // Create a map
+                    Map = new Map(_basemap);
+
+                    // Create a graphics overlay
+                    _geonamesOverlay = CreateGeonamesOverlay();
+
+                    // Add it to the map view
+                    Overlays = new GraphicsOverlayCollection();
+                    Overlays.Add(_geonamesOverlay);
+                    break;
+
+                default:
+                    // TODO: Error handling
+                    break;
+            }
+        }
+
+        private static GraphicsOverlay CreateGeonamesOverlay()
+        {
+            var overlay = new GraphicsOverlay();
+            overlay.Renderer = CreateGeonamesRenderer();
+            return overlay;
+        }
+
+        private static Renderer CreateGeonamesRenderer()
+        {
+            var geonamesSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Colors.Black, 3);
+            return new SimpleRenderer(geonamesSymbol);
+        }
+
+        /// <summary>
+        /// Gets or sets the map
+        /// </summary>
+        public Map Map
+        {
+            get { return _map; }
+            set
+            {
+                _map = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the graphics overlays.
+        /// </summary>
+        public GraphicsOverlayCollection Overlays
+        {
+            get { return _overlays; }
+            set
+            {
+                _overlays = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Raises the <see cref="MapViewModel.PropertyChanged" /> event
+        /// </summary>
+        /// <param name="propertyName">The name of the property that has changed</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var propertyChangedHandler = PropertyChanged;
+            if (propertyChangedHandler != null)
+                propertyChangedHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
